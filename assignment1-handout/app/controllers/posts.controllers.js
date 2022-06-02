ed2bd591be7fe154197241958b453a7b1ee48618
Ben Zunker <bpzunker@gmail.com>
2022-06-01 19:29:56 -0700

    Primitive posting working!

--- assignment1-handout/app/controllers/posts.controllers.js
+++ assignment1-handout/app/controllers/posts.controllers.js
@@ -1,37 +1,64 @@
 const Post = require("../models/post");
+require('dotenv').config('../config/config.js');
+const config = require('../config/config');
+const AWS = require('aws-sdk');
+AWS.config.update({
+    accessKeyId: config.get('dynamo.key'),
+    secretAccessKey: config.get('dynamo.secret'),
+    "region": 'us-west-2'
+});
+var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
 
 
 const homeStartingContent =
 	'The home pages lists all the blogs from all the users.';
 
 const composePost = (req, res) => {
-	const post = new Post({
+	/* const post = new Post({
     username: req.user.username,
 		title: req.body.postTitle,
 		content: req.body.postBody
 	});
 
 	post.save();
-	res.redirect('/post');
+	res.redirect('/post'); */
+	var params = {
+		TableName: config.get('dynamo.name'),
+		Item: {
+		  'postId' : {S: '000'},
+		  'username' : {S: req.user.username},
+		  'title' : {S: req.body.postTitle}, 
+		  'content' : {S: req.body.postBody} 
+		}
+	  };
+	
+	ddb.putItem(params, function(err, data) {
+	  if (err) {
+		console.log("Error", err);
+	  } else {
+		console.log("Success", data);
+	  }
+	});
 };
 
 const displayAllPosts = (req, res) => {
-	Post.find({}, function(err, posts) {
+	/* Post.find({}, function(err, posts) {
 		res.render('home', {
 			startingContent: homeStartingContent,
 			posts: posts
 		});
-	});
+	}); */
 };
 async function displayPost (req, res)  {
 	const requestedPostId = req.params.postId;
 
-	Post.findOne({ _id: requestedPostId }, function(err, post) {
+	/* Post.findOne({ _id: requestedPostId }, function(err, post) {
 		res.render('post', {
 			title: post.title,
 			content: post.content
 		});
-	});
+	}); */
 };
 
 module.exports = {
